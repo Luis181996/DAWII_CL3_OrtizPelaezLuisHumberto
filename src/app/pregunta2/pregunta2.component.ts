a01a4c54d215feb06386da660a2acf60bab52db5
Luis181996 <luishumberto181996@hotmail.com>
2024-07-03 21:45:45 -0500

    pregunta 2 finalizada

--- src/app/pregunta2/pregunta2.component.ts
+++ src/app/pregunta2/pregunta2.component.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { pregunta2 } from './post2';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Pregunta2Service } from './post2.service';
+import { MaterialModule } from '../angular-material/material/material.module';
 
 @Component({
   selector: 'app-pregunta2',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './pregunta2.component.html',
   styleUrl: './pregunta2.component.css'
 })
 export class Pregunta2Component {
   post: pregunta2[] = [];
-  displayedColumns: string[] = ["Id", "Name", "Status", "Species"];
+  displayedColumns: string[] = ["id", "name", "status", "species"];
 
   constructor(private postService: Pregunta2Service,
     private route: ActivatedRoute,
